8f4b8f2a4ece5160e36d3ed0fb672c864f59accc
saraavdic <153551548+saraavdic@users.noreply.github.com>
2024-07-14 23:03:12 +0200

    Add files via upload

---
+++ MERN-JS-P1-calculator-main/MERN-JS-P1-calculator-main/README.md
@@ -0,0 +1,1 @@
+# MERN-JS-P1-calculator
---
+++ MERN-JS-P1-calculator-main/MERN-JS-P1-calculator-main/js-course-start/assets/scripts/app.js
@@ -0,0 +1,12 @@
+function zbir(a,b){
+    return(a+b);
+}
+function razlika(a,b){
+    return(a-b);
+}
+function proizvod(a,b){
+    return(a*b);
+}
+function kolicnik(a,b){
+    return(a/b);
+}
